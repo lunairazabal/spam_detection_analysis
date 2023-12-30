970880152fcbdf15fddb6b403f45bb94b90414a9
Luna <lunaira21@hotmail.com>
2023-12-30 14:04:53 +0100

    Upgrade app

--- src/inference.py
+++ src/inference.py
@@ -1,21 +1,39 @@
-import os
-import pickle
+# import os
+# import pickle
 
-MODELS_PATH = 'models'
+# MODELS_PATH = 'models'
 
-def transform_data(data):
-    # utilizar el mismo pipeline de ETL que se creó en el notebook de ETL
+# def transform_data(data):
+#     # utilizar el mismo pipeline de ETL que se creó en el notebook de ETL
+#     # transformar los datos
+#     return data
+
+# def predict(data_processed, model_type='ml'):
+#     # cargar el modelo
+#     if model_type == 'ml':
+#         model = pickle.load(open(os.path.join(MODELS_PATH, 'xg_boost_model.pkl'), 'rb'))
+#     else:
+#         raise ValueError("Tipo de modelo no reconocido. Utiliza 'ml' para modelos de machine learning.")
+
+#     prediction = model.predict(data_processed)
+
+#     return prediction
+
+from inference import transform_data, predict
+import streamlit as st
+
+
+
+# cuando le den al boton de predecir llamamos primero a transform_data y luego a predict
+
+text_input = st.text_input('Introduce un texto en inglés')
+
+if btn_predict:
     # transformar los datos
-    return data
+    data = transform_data(text_input)
 
-def predict(data_processed, model_type='ml'):
-    # cargar el modelo
-    if model_type == 'ml':
-        model = pickle.load(open(os.path.join(MODELS_PATH, 'xg_boost_model.pkl'), 'rb'))
-    else:
-        raise ValueError("Tipo de modelo no reconocido. Utiliza 'ml' para modelos de machine learning.")
+    # predecir
+    prediction = predict(data)
 
-    prediction = model.predict(data_processed)
-
-    return prediction
-
+    # mostrar el resultado
+    st.write(prediction)
